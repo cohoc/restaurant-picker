be6aa47c4092376bc5ce3fefb4c6ba6665d31429
cohoc <chris.carrillocs@gmail.com>
2022-03-24 22:31:44 -0500

    Changed search bar styling

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useContext} from 'react'
 import { MapContext } from '../Context/MapContext'
+import BasicMarker from '../../assets/svg/marker-basic.svg'
 import useGoogle from "react-google-autocomplete/lib/usePlacesAutocompleteService"
 //import Autocomplete from "react-google-autocomplete";
 import './Searchbar.css'
@@ -73,10 +74,16 @@ function Searchbar() {
                         {placePredictions?.map( (item, index) => {
                             return(
                                 <li 
+                                    className="search-item"
                                     onClick={ () => {onPlaceSelected(item); clear()} }
                                     key={index}>
-
-                                    {item.description}
+                                    <div className="item-icon-container">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="search-item-icon" viewBox="0 0 24 24">
+                                            <path d="M11.513 12.38c-2.117 0-3.835-1.729-3.835-3.862 0-2.135 1.718-3.863 3.835-3.863s3.835 1.729 3.835 3.863c0 2.132-1.718 3.862-3.835 3.862m0-12.38C6.825 0 3.025 3.827 3.025 8.549c0 4.46 3.844 10.213 6.411 13.014.959 1.045 2.076 2.454 2.076 2.454s1.2-1.417 2.229-2.493C16.306 18.84 20 13.451 20 8.549 20 3.827 16.2 0 11.513 0"/>
+                                        </svg>
+                                    </div>
+                                    <span className="searchbar-divider"></span>
+                                    <p>{item.description}</p>
                                 </li>
                             )
                         })}
