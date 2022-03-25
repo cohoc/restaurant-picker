216fb2b25a7165abb4507014b34099597fba3888
cohoc <chris.carrillocs@gmail.com>
2022-03-25 05:24:30 -0500

    Added section for next list of restaurants

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -16,10 +16,6 @@ function List() {
                 Choose your restaurant
             </h2>
 
-            <p>{filter.rating}</p>
-            <p>{filter.miles}</p>
-            <p>{filter.price}</p>
-
             <div className="list-filters-container">
 
                 <Dropdown
@@ -122,6 +118,24 @@ function List() {
                         : ('')}
                     </ul>
                     
+                </div>
+
+                
+
+            </div>
+            <div className="list-page-container">
+                <p>Showing 1-20</p>
+                <div className="list-page-buttons">
+                    <button className="page-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="button-icon" viewBox="0 0 512 512">
+                            <path d="M213.7 256 380.9 81.9c4.2-4.3 4.1-11.4-.2-15.8l-29.9-30.6c-4.3-4.4-11.3-4.5-15.5-.2L131.1 247.9c-2.2 2.2-3.2 5.2-3 8.1-.1 3 .9 5.9 3 8.1l204.2 212.7c4.2 4.3 11.2 4.2 15.5-.2l29.9-30.6c4.3-4.4 4.4-11.5.2-15.8L213.7 256z"/>
+                        </svg>
+                    </button>
+                    <button className="page-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="button-icon" viewBox="0 0 512 512">
+                            <path d="M298.3 256 131.1 81.9c-4.2-4.3-4.1-11.4.2-15.8l29.9-30.6c4.3-4.4 11.3-4.5 15.5-.2L380.9 248c2.2 2.2 3.2 5.2 3 8.1.1 3-.9 5.9-3 8.1L176.7 476.8c-4.2 4.3-11.2 4.2-15.5-.2L131.3 446c-4.3-4.4-4.4-11.5-.2-15.8L298.3 256z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
